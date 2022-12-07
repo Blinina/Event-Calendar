8b4558fb1129e77016de82bb7331dc20e49335ed
Blinina <anastasia.paancake@gmail.com>
2022-12-07 21:10:23 +0300

    added css and day-select

--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -1,13 +1,15 @@
-import { React, useState } from "react";
-import { Table, Form } from 'react-bootstrap';
+import { React, useState, useEffect  } from "react";
+import { Table, Form, Button } from 'react-bootstrap';
 import createCalendar from "../createCalendar";
 
 export default function Main() {
+    const [yearCalendar, setYearCalendar] = useState(false);
+    const [monthCalendar, setMonthCalendar] = useState(true);
+
     const [currentMonth, setCurrentMonth] = useState(new Date(Date.now()).getMonth());
     const [currentYear, setCurrentYear] = useState(new Date(Date.now()).getFullYear());
     const [currentDay, setCurrentDay] = useState(new Date(Date.now()).getDate());
 
-console.log(new Date(Date.now()))
     // const nextMonth = new Date(year, month + 1);
     const daysArr = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
     const monthArr = ['January', 'February', 'March', 'April', 'May',
@@ -15,38 +17,44 @@ console.log(new Date(Date.now()))
 
     const yearsArr = [2020, 2021, 2022, 2023, 2024];
 
-    const calendar = createCalendar(currentYear, currentMonth);
- 
-   const handleDay  = (el) =>{
-    console.log(el);
-    setCurrentDay(el)
-
-   }
-   const handleYear = (e) =>{
-   setCurrentYear(e.target.value)
-   }
-   const  handleMonth  = (e) => {
-    setCurrentMonth(monthArr.indexOf(e.target.value))
+    const handleDay = (el) => {
+        setCurrentDay(el);
     }
+    const handleYear = (e) => {
+        setCurrentYear(e.target.value)
+    }
+    const handleMonth = (e) => {
+        setCurrentMonth(monthArr.indexOf(e.target.value))
+    }
+    // const handleDefault = () => {
+      
+
+    // }
+    // const handleYear = () => {
+        
+    // };
+     const calendar = createCalendar( currentYear, currentMonth)
 
     return (<div>
         <div className="header">
             <Form.Select
-            name="monthSelect"
-            value={monthArr[currentMonth]}
-            onChange={handleMonth}>
+                name="monthSelect"
+                value={monthArr[currentMonth]}
+                onChange={handleMonth}>
                 {monthArr.map((el) => <option key={el} value={el}>{el}</option>)}
             </Form.Select>
             <Form.Select
-             name="yearSelect"
-             value={currentYear}
-             onChange={(e) =>handleYear(e)} >
+                name="yearSelect"
+                value={currentYear}
+                onChange={(e) => handleYear(e)} >
                 {yearsArr.map((el) => <option key={el} value={el}>{el}</option>)}
             </Form.Select>
+            {/* <Button onClick={handlePrevious}>Month</Button>
+            <Button onClick={handleNext}>Year</Button> */}
         </div>
-        <h2> {currentDay} : {currentMonth+1} : {currentYear} </h2>
-        <Table striped bordered hover>
-            <thead>
+        <h2> {currentDay} : {monthArr[currentMonth]} : {currentYear} </h2>
+        <Table striped bordered hover className="table">
+            <thead >
                 <tr>
                     {daysArr.map((el) => <th key={el}>{el}</th>)}
                 </tr>
@@ -54,11 +62,12 @@ console.log(new Date(Date.now()))
             <tbody>
                 {calendar.map((week, index) => (
                     <tr key={index}>
-                        {week.map((el, i) => el ? <td onClick={()=>handleDay(el.getDate())} key={i}>{el.getDate()}</td> : <td key={i} />)}
+                        {week.map((el, i) => el ? <td className={el.getDate()===currentDay ? 'selectTD' : 'white'} onClick={() => handleDay(el.getDate())} key={i}>{el.getDate()}</td> : <td key={i} />)}
                     </tr>
                 ))}
             </tbody>
         </Table>
+        
     </div>
     )
 }
